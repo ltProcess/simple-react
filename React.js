baa149cb66616a8713133606152d351a873029ab
longteng <longteng@xiaomi.com>
2018-12-11 16:49:36 +0800

    <feat> render string by simple-react

---
+++ React.js
@@ -0,0 +1,19 @@
+function createElement(parentEle, props, childEle) {
+  let parentElement = document.createElement(parentEle)
+
+  parentElement.innerHTML = childEle
+
+  return parentElement
+}
+
+function render(insertEle, rootEle) {
+  rootEle.appendChild(insertEle)
+}
+
+React = {
+  createElement
+}
+
+ReactDOM = {
+  render
+}
